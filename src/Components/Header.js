9b7271e6d517caf6a505b17f703567d90c992120
Vitali <ThePursuer@mail.ru>
2022-04-13 12:53:12 +0300

    13.04.2022.2

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
-import {Navbar, Nav, Container, Modal, Form, Button, FormControl, Row} from "react-bootstrap";
+import {Navbar, Nav, Container, Modal, Form, Button, FormControl, Row, NavDropdown} from "react-bootstrap";
 
 
 const Header = ({light}) => {
 
     return (
-        <Row>
-            <Navbar  collapseOnSelect expand={'md'} variant={light} bg={light}>
+            <Navbar  collapseOnSelect expand="lg" variant={light} bg={light}>
                 <Container>
                     <Navbar.Brand className = 'contents' href='/' >
                         <div className={`${light} logo`}>
@@ -16,24 +15,27 @@ const Header = ({light}) => {
                             </div>
                         </div>
                     </Navbar.Brand>
-                    <Nav className={'m-auto'}>
-                        <Nav.Link className={'link'} href={'/'}>
-                            HOME
-                        </Nav.Link>
-                        <Nav.Link className={'link'}  href={'/setting'}>
-                            SETTING
-                        </Nav.Link>
-                        <Nav.Link className={'link'}  href={'/help'}>
-                            HELP
-                        </Nav.Link>
-                        <Nav.Link className={'link'}  href={'/about_us'}>
-                            ABOUT US
-                        </Nav.Link>
-                    </Nav>
-
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className={'m-auto'}>
+                            <Nav.Link className={'link'} href={'/'}>
+                                HOME
+                            </Nav.Link>
+                            <Nav.Link className={'link'}  href={'/setting'}>
+                                SETTING
+                            </Nav.Link>
+                            <Nav.Link className={'link'}  href={'/help'}>
+                                HELP
+                            </Nav.Link>
+                            <Nav.Link className={'link'}  href={'/about_us'}>
+                                ABOUT US
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
-        </Row>
+
+        
 
     );
 };
